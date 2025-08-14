d295344d0515b64bbad34843eb6d913376f3a7f2
Daniel Urbina <edu211004@gmail.com>
2025-08-13 18:13:00 -0600

    basic route

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hola')
+const express = require('express')
+const app = express()
+
+const PORT = 3001
+
+app.get('', (req, res) => {
+  console.log('request a ruta principal')
+})
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`)
+})
