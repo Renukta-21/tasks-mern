4a2cf7a0a31ce2ef3d8e7a1a1adb21481d173145
Daniel Urbina <edu211004@gmail.com>
2025-08-13 19:02:34 -0600

    rendering tasks

--- client/src/Tasks.jsx
+++ client/src/Tasks.jsx
@@ -1,8 +1,16 @@
 function Tasks() {
-    const tasks = ['task 1', 'task 2', 'task 3']
-    
-    return (
-    <div>Add a task it will appear here!</div>
+  const tasks = ['task 1', 'task 2', 'task 3']
+
+  return (
+    <div>
+      {tasks.length > 0 ? (
+        <p>{tasks.map(task=>(
+            <div>{task}</div>
+        ))}</p>
+      ) : (
+        <p>There is no tasks added yet, add one to get started!</p>
+      )}
+    </div>
   )
 }
 
