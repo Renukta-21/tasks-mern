97ef50a4082662e845fb03248a5d8ee627522735
Daniel Urbina <edu211004@gmail.com>
2025-08-13 18:56:13 -0600

    tasks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
+import Tasks from "./Tasks"
+
 function App() {
   return (
-    <div className="text-3xl text-black">App</div>
+    <div className="text-3xl text-black">
+      <h2>Task app</h2>
+      <Tasks/>
+    </div>
   )
 }
 
---
+++ client/src/Tasks.jsx
@@ -0,0 +1,9 @@
+function Tasks() {
+    const tasks = ['task 1', 'task 2', 'task 3']
+    
+    return (
+    <div>Add a task it will appear here!</div>
+  )
+}
+
+export default Tasks
