4be348cafc9888923e4bc46a78d94281e94fbc32
Daniel Urbina <edu211004@gmail.com>
2025-08-28 22:06:05 -0600

    fetching

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const {v4} = require('uuid')
+const cors = require('cors')
 const app = express()
 
 const PORT = 3001
 
+const tasks = [
+  { title: 'task 1', id: v4(), completed: true },
+  { title: 'task 2', id: v4(), completed: false },
+  { title: 'task 3', id: v4(), completed: true },
+]
+
+const mainGreeting = `
+  <h2>API Routes</h2>
+  <ul>
+  <li>/</li>
+  <li><strong>/tasks </strong> Retrieve all tasks</li>
+  </ul>
+`
+app.use(cors())
 app.get('', (req, res) => {
   console.log('request a ruta principal')
-  res.end()
+  res.send(mainGreeting)
+})
+app.get('/tasks', (req,res)=>{
+  console.log('/tasks GET')
+  res.json(tasks)
 })
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`)
